b056d2dddc25a853951fa1a755909e4339c45a1b
Lesha07 <simchuklesha@gmail.com>
2024-12-30 01:28:09 +0200

    11

--- HomeWork3.4/Product.cs
+++ HomeWork3.4/Product.cs
@@ -2,5 +2,8 @@ namespace HomeWork3._4;
 
 public class Product
 {
-    
+    public string Name { get; set; }
+    public string Category { get; set; }
+    public int Price { get; set; }
 }
+  
--- HomeWork3.4/Program.cs
+++ HomeWork3.4/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HomeWork3._4;
+//ПП - правильное питание
+//ФФ - фаст-фуд
+var products = new List<Product>
+{
+    new Product{Name = "Огурец", Category = "ПП", Price = 10},  
+    new Product{Name  = "Попмидор", Category = "ПП", Price = 14},
+    
+    new Product {Name = "Сникерс", Category = "ФФ", Price = 40},
+    new Product {Name = "Бурито", Category = "ФФ", Price = 70},
+};
+
+var filterForFF = products.Where(p => p.Category == "ФФ");
+
+var filterForPP = products.Where(p => p.Category == "ПП");
+
+double averagePricePP = filterForPP.Average(p => p.Price);
+double averagePriceFF = filterForFF.Average(p => p.Price);
+
+foreach (var ProductFF in filterForFF)
+{
+    Console.WriteLine($"{ProductFF.Name}, {ProductFF.Category}, {ProductFF.Price}");
+}
+
+Console.WriteLine($"Средняя стоимость фаст-фуд товаров: {averagePriceFF}");
+
+foreach (var ProductPP in filterForPP)
+{
+    Console.WriteLine($"{ProductPP.Name}, {ProductPP.Category}, {ProductPP.Price}");
+}
+
+Console.WriteLine($"Средняя стоимость полезный товаров: {averagePricePP}");
